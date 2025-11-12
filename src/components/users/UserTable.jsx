53534d74d5e8b16a0eeef88fa63d2c0678c1fbb0
Aleek <2023371035@uteq.edu.mx>
2025-11-12 14:12:25 -0600

    feat (usuarios): Se implementó el modal para cambiar el rol del usuario.

--- src/components/users/UserTable.jsx
+++ src/components/users/UserTable.jsx
@@ -13,7 +13,8 @@ import { X } from "lucide-react";
 
 export function UserTable({ users, isLoading, error }) {
 
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen: isOpenDelete, onOpen: onOpenDelete, onClose: onCloseDelete } = useDisclosure()
+    const { isOpen: isOpenRole, onOpen: onOpenRole, onClose: onCloseRole } = useDisclosure()
     const [selectedUserId, setSelectedUserId] = useState(null);
 
     if (isLoading) {
@@ -30,7 +31,12 @@ export function UserTable({ users, isLoading, error }) {
 
     const handleDeleteClick = (userId) => {
         setSelectedUserId(userId);
-        onOpen();
+        onOpenDelete();
+    }
+
+    const handleRoleClick = (userId) => {
+        setSelectedUserId(userId);
+        onOpenRole();
     }
 
     return (
@@ -53,13 +59,14 @@ export function UserTable({ users, isLoading, error }) {
                                 <Td color="secondary-default">
                                     <Tooltip label="Cambiar rol del usuario" placement="top">
                                     <Button
+                                        onClick={() => handleRoleClick(user.id)}
                                         color="secondary-default"
                                         bg="transparent"
                                         _hover={{ bg: 'transparent', color: 'accent-default' }}>
                                     {user.role}
                                     </Button>
                                     </Tooltip>
-                                    </Td>
+                                </Td>
                                 <Td color="secondary-default">
                                     <Button
                                         onClick={() => handleDeleteClick(user.id)}
@@ -82,21 +89,38 @@ export function UserTable({ users, isLoading, error }) {
                 </Table>
             </TableContainer>
 
-            <Modal isOpen={isOpen} onClose={onClose} isCentered>
+            {/* Modal para eliminar */}
+            <Modal isOpen={isOpenDelete} onClose={onCloseDelete} isCentered>
                 <ModalOverlay />
-                <ModalContent
-                alignItems="center">
+                <ModalContent alignItems="center">
                     <ModalHeader>¿Desea eliminar a este usuario?</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
                         Haga clic en "Aceptar" para confirmar la eliminación del usuario.
                     </ModalBody>
-                    <ModalFooter
-                    justifyContent="center">
-                        <Button colorScheme='red' mr={3} onClick={onClose}>
+                    <ModalFooter justifyContent="center">
+                        <Button colorScheme='red' mr={3} onClick={onCloseDelete}>
                             Cancelar
                         </Button>
                         <Button variant='outline'>Eliminar</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+
+            {/* Modal para cambiar rol */}
+            <Modal isOpen={isOpenRole} onClose={onCloseRole} isCentered>
+                <ModalOverlay />
+                <ModalContent alignItems="center">
+                    <ModalHeader>¿Desea cambiar el rol del usuario?</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        Selecciona el nuevo rol para este usuario.
+                    </ModalBody>
+                    <ModalFooter justifyContent="center">
+                        <Button colorScheme="blue" mr={3} onClick={onCloseRole}>
+                            Usuario
+                        </Button>
+                        <Button variant='outline'>Administrador</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
