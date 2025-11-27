37e04a7c8c44be73b8f474f98b41fe099c57f420
juanCordero99 <juan.pablo.cor.sot@gmail.com>
2025-11-27 11:52:33 -0600

    fix: Backend and front-end integrated, routes protected. Please use Admin account to access

--- src/hooks/useFetchDocuments.jsx
+++ src/hooks/useFetchDocuments.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-// import {getAllDocuments, getDocumentsByUserId} from '../services/documentService';
-import { getAllDocumentsMock as getAllDocuments, getDocumentsByUserIdMock as getDocumentsByUserId } from '../services/mockDocumentService';
+import {getAllDocuments, getDocumentsByUserId} from '../services/documentService';
 import localforage from "localforage";
 
 
@@ -90,4 +89,4 @@ export function useFetchDocumentsForUser() {
     }, []);
 
     return { documents, pagination, isLoading, error };
-}
+};
