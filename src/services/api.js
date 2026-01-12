1b4187b771ada87f6b3ecbcee9ff8e7cb390f833
JuanCordero1024 <2023371024@uteq.edu.mx>
2026-01-12 10:27:15 -0600

    Update api.js Wrong route

--- src/services/api.js
+++ src/services/api.js
@@ -9,7 +9,7 @@ const USER_ID = 'user_id';
 const USER_NAME = 'user_name';
 // 2. Crea la instancia de axios
 const api = axios.create({
-    baseURL: 'https://theworkersapigateway.zeabur.app'
+    baseURL: 'http://theworkersapigateway.zeabur.internal'
 });
 
 api.interceptors.request.use(
